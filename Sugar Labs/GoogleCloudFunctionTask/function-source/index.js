c4806bc0d52eaa79ab8ac9e3e80f66a8fb8c992b
Ivan Chau <ivanmchau@gmail.com>
2017-12-29 15:36:56 -0500

    ayy

--- Sugar Labs/GoogleCloudFunctionTask/function-source/index.js
+++
@@ -1,4 +0,0 @@
-// link for HTTP Trigger : https://us-central1-sugar-labs.cloudfunctions.net/HelloWorldFunction  
-exports.helloWorld = function helloWorld(req, res) {
-    res.status(200).send('Hello World!');
-};
